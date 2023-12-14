b2994fdbb6fa4bc64511d63d762756f718012d99
Sergey Kuran <kuran.serg@gmail.com>
2023-12-14 08:38:54 +0200

    Edit name secret key

--- helpers/authenticate.js
+++ helpers/authenticate.js
@@ -2,7 +2,7 @@ import { HttpError } from './Error/HttpError.js';
 import jwt from 'jsonwebtoken';
 import { User } from '../models/user.js';
 
-const { JWT_SECRET } = process.env;
+const { SECRET_KEY } = process.env;
 
 const authenticate = async (req, res, next) => {
   const { authorization = '' } = req.headers;
@@ -11,7 +11,7 @@ const authenticate = async (req, res, next) => {
   if (bearer !== 'Bearer') throw HttpError(401);
 
   try {
-    const { id } = jwt.verify(token, JWT_SECRET);
+    const { id } = jwt.verify(token, SECRET_KEY);
     const user = await User.findById(id);
     if (!user || !user.token) throw HttpError(401);
 
