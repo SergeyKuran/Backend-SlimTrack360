778553ae078ef0f7c13963d5e0bbce1b15955041
Sergey Kuran <kuran.serg@gmail.com>
2023-12-15 21:30:25 +0200

    Delete import from user-route

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,7 +1,4 @@
 import { Router } from 'express';
-import userSchemas from '../schemas/userSchemas.js';
-import { bodyValidator } from '../decorators/bodyValidator.js';
-import authController from '../controllers/authController.js';
 import userController from '../controllers/userController.js';
 import authenticate from '../helpers/authenticate.js';
 
