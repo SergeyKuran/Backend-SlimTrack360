b5c8b9d8ec9c27624437fc32a7c3922263d52032
Sergey Kuran <kuran.serg@gmail.com>
2023-12-15 21:30:02 +0200

    Update code from authenticate

--- helpers/authenticate.js
+++ helpers/authenticate.js
@@ -8,12 +8,16 @@ const authenticate = async (req, res, next) => {
   const { authorization = '' } = req.headers;
   const [bearer, token] = authorization.split(' ');
 
-  if (bearer !== 'Bearer') throw HttpError(401);
+  if (bearer !== 'Bearer') {
+    throw HttpError(401);
+  }
 
   try {
     const { id } = jwt.verify(token, SECRET_KEY);
     const user = await User.findById(id);
-    if (!user || !user.token) throw HttpError(401);
+    if (!user || !user.token) {
+      throw HttpError(401);
+    }
 
     req.user = user;
 
