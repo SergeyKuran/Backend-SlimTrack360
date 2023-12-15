f52e4516706a60270d64c40a4fd4f865e2fb6763
Sergey Kuran <kuran.serg@gmail.com>
2023-12-15 21:29:10 +0200

    Update userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,35 @@
 import { User } from '../models/user.js';
 import { ctrlWrapper } from '../decorators/ctrlWrapper.js';
 import { HttpError } from '../helpers/Error/HttpError.js';
-import authServices from '../services/auth-services.js';
 import userServices from '../services/user-services.js';
 
 const current = async (req, res, next) => {
-  const user = await userServices.currentUser(req.user);
+  console.log('req.user.name :>> ', req.user.name);
+  const {
+    name,
+    email,
+    avatarUrl,
+    sex,
+    age,
+    height,
+    currentWeight,
+    dailyGoalCalories,
+    dailtyGoalWater,
+    dailyGoalElements,
+  } = await userServices.currentUser(req.user._id);
+
+  res.json({
+    name,
+    email,
+    avatarUrl,
+    sex,
+    age,
+    height,
+    currentWeight,
+    dailyGoalCalories,
+    dailtyGoalWater,
+    dailyGoalElements,
+  });
 };
 
 export default {
