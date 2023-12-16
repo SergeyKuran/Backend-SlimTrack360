81ac3e136a139052dbb5f1751ba9ba9a3fd08b48
Sergey Kuran <kuran.serg@gmail.com>
2023-12-16 19:05:08 +0200

    Update user-controller
    add update controller
    add goal controller
    add weight controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,7 @@
-import { User } from '../models/user.js';
 import { ctrlWrapper } from '../decorators/ctrlWrapper.js';
-import { HttpError } from '../helpers/Error/HttpError.js';
 import userServices from '../services/user-services.js';
 
 const current = async (req, res, next) => {
-  console.log('req.user.name :>> ', req.user.name);
   const {
     name,
     email,
@@ -32,6 +29,56 @@ const current = async (req, res, next) => {
   });
 };
 
+const update = async (req, res, next) => {
+  const {
+    name,
+    email,
+    avatarUrl,
+    sex,
+    age,
+    height,
+    currentWeight,
+    dailyGoalCalories,
+    dailtyGoalWater,
+    dailyGoalElements,
+  } = await userServices.updateUser(req.user._id, req.body);
+
+  res.json({
+    name,
+    email,
+    avatarUrl,
+    sex,
+    age,
+    height,
+    currentWeight,
+    dailyGoalCalories,
+    dailtyGoalWater,
+    dailyGoalElements,
+  });
+};
+
+const goal = async (req, res, next) => {
+  const { goal } = await userServices.goalUser(req.user._id, req.body.goal);
+
+  res.json({
+    goal,
+  });
+};
+
+const weight = async (req, res, next) => {
+  const { currentWeight } = await userServices.weightUser(
+    req.user._id,
+    req.body.currentWeight,
+  );
+
+  res.json({
+    currentWeight,
+  });
+};
+
 export default {
   current: ctrlWrapper(current),
+  update: ctrlWrapper(update),
+  goal: ctrlWrapper(goal),
+  weight: ctrlWrapper(weight),
 };
