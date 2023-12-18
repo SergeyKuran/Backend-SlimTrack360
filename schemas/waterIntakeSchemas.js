7cbacdb7dcd2173095081ffcc63af0a68f13e8fd
MaksymBora <maxboraod@gmail.com>
2023-12-18 16:27:37 +0100

    update waterIntake

--- schemas/waterIntakeSchemas.js
+++ schemas/waterIntakeSchemas.js
@@ -1,13 +1,13 @@
 import Joi from 'joi';
 
-const errorMessages = {
-  'string.base': '{#label} must be a string',
-  'string.empty': '{#label} cannot be empty',
-};
+// const errorMessages = {
+//   // 'string.base': '{#label} must be a string',
+//   // 'string.empty': '{#label} cannot be empty',
+// };
 
 export const AddWaterIntakeSchema = Joi.object({
   date: Joi.date().max('now').required(),
-  value: Joi.number().required().messages(errorMessages),
+  value: Joi.number().optional(),
 });
 
 export const getWaterIntakeSchema = Joi.object({
