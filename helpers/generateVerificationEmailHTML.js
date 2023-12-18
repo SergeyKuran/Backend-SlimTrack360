27bf2ecd22d98d2d5eef2e604383970fb11f3b77
Sergey Kuran <kuran.serg@gmail.com>
2023-12-18 21:28:30 +0200

    Creane func generateVerificationEmailHTML

---
+++ helpers/generateVerificationEmailHTML.js
@@ -0,0 +1,35 @@
+export const generateVerificationEmailHTML = (verificationToken, BASE_URL) => `
+  <html>
+    <head>
+      <style>
+        body {
+          font-family: 'Arial', sans-serif;
+        }
+        .container {
+          max-width: 600px;
+          margin: 0 auto;
+          padding: 20px;
+        }
+        .verification-link {
+          display: block;
+          padding: 10px;
+          margin: 20px 0;
+          background-color: #4CAF50;
+          color: white;
+          text-align: center;
+          text-decoration: none;
+          font-size: 16px;
+          border-radius: 5px;
+        }
+      </style>
+    </head>
+    <body>
+      <div class="container">
+        <h2>Verify Your Email</h2>
+        <p>Click the following link to verify your email address:</p>
+        <a class="verification-link" href="${BASE_URL}/api/auth/verify/${verificationToken}" target="_blank">Verify Email</a>
+        <p>If you did not sign up for our service, you can safely ignore this email.</p>
+      </div>
+    </body>
+  </html>
+`;
