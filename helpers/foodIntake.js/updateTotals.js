098622357113d35c7eb32617d27ba9ba9fd4451d
MaksymBora <maxboraod@gmail.com>
2023-12-18 02:36:49 +0100

    update calculate calories and add field totalCalories for a day

--- helpers/foodIntake.js/updateTotals.js
+++ helpers/foodIntake.js/updateTotals.js
@@ -1,5 +1,38 @@
+// export const updateIntakeTotals = async intake => {
+//   const sections = ['breakfast', 'lunch', 'dinner', 'snack'];
+
+//   for (const section of sections) {
+//     const products = intake[section].products;
+
+//     const total = products.reduce(
+//       (acc, curr) => {
+//         acc.totalCalories += curr.calories || 0;
+//         acc.totalFat += curr.fat || 0;
+//         acc.totalCarbonohidretes += curr.carbonohidretes || 0;
+//         acc.totalProtein += curr.protein || 0;
+//         return acc;
+//       },
+//       {
+//         totalCalories: 0,
+//         totalFat: 0,
+//         totalCarbonohidretes: 0,
+//         totalProtein: 0,
+//       },
+//     );
+
+//     intake[section].totalCalories = total.totalCalories;
+//     intake[section].totalFat = total.totalFat;
+//     intake[section].totalCarbonohidretes = total.totalCarbonohidretes;
+//     intake[section].totalProtein = total.totalProtein;
+//   }
+
+//   await intake.save();
+// };
+
 export const updateIntakeTotals = async intake => {
   const sections = ['breakfast', 'lunch', 'dinner', 'snack'];
+
+  let totalAllCalories = 0;
 
   for (const section of sections) {
     const products = intake[section].products;
@@ -24,7 +57,13 @@ export const updateIntakeTotals = async intake => {
     intake[section].totalFat = total.totalFat;
     intake[section].totalCarbonohidretes = total.totalCarbonohidretes;
     intake[section].totalProtein = total.totalProtein;
+
+    // Суммируем общее количество калорий из каждого раздела
+    totalAllCalories += total.totalCalories;
   }
+
+  // Записываем общее количество калорий в поле totalCalories главного объекта
+  intake.totalCalories = totalAllCalories;
 
   await intake.save();
 };
