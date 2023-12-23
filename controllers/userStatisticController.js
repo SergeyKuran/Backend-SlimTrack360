3aacbd45335d284ef4dab5be22efa738835da4b1
Sergey Kuran <kuran.serg@gmail.com>
2023-12-23 20:16:48 +0200

    Update

--- controllers/userStatisticController.js
+++ controllers/userStatisticController.js
@@ -81,7 +81,7 @@ const getUserStatistic = async (req, res, next) => {
 
   const user = await UserWeight.findOne({ owner });
 
-  const monthData = user[monthName] || 0;
+  const monthData = user[monthName] || [];
 
   res.status(200).json({
     totalWater,
