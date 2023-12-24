6381d4a79ee9e25e7655df0242a52a008f837f0c
Sergey Kuran <kuran.serg@gmail.com>
2023-12-24 10:15:44 +0200

    Fix

--- decorators/bodyValidator.js
+++ decorators/bodyValidator.js
@@ -2,7 +2,7 @@ import { HttpError } from '../helpers/Error/HttpError.js';
 
 export const bodyValidator = schema => (req, res, next) => {
   const { error } = schema.validate(req.body, { abortEarly: false });
-  console.log('TEST VALIDATION BODY>>>>>>>', req.body);
+
   if (typeof error !== 'undefined') {
     const errorMessages = error.details.map(err => `${err.message}`).join(', ');
     return res.status(400).json({ message: errorMessages });
